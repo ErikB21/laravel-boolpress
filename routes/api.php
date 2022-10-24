c1a5e692d5df860a6c1a00c68ac5a739e397c85a
ErikBorgogno <erikborgo.20@gmail.com>
2022-10-24 12:05:38 +0200

    add route method post

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('posts', 'Api\PostController@index');
 Route::get('posts/{slug}', 'Api\PostController@show');
 
+Route::post('contacts', 'Api\MailController@store');
+
 
