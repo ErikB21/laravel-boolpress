67a7d8442b58416d48baf603675aa3428c6b3605
ErikBorgogno <erikborgo.20@gmail.com>
2022-10-24 12:06:47 +0200

    add new view for response

---
+++ resources/views/emails/newContact.blade.php
@@ -0,0 +1,8 @@
+<h1>Salve Amministratore</h1>
+
+<p>Hai ricevuto un nuovo messaggio di posta:</p>
+<p>
+    Nome: {{$lead->name}} <br>
+    Mail: {{$lead->mail}} <br>
+    Message: {{$lead->message}}
+</p>
